199c3ebe4c3f8f9cf85da8d72ac5dc87536dced1
sasakiso <s.sasaki@corekara.co.jp>
2026-09-21 20:25:47 +0900

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,591 @@
+//生徒作品
+const materials_students = [
+    {
+        title: "生徒作品（募集中！）",
+        materials: [
+            {
+                id: "S-01",
+                student_name: "これから太郎",
+                school: "サンプル教室",
+                name: "Scratchのプロジェクト",
+                link: "projects/s/S-01.html",
+                memo: "ああああああああああああああああああああああああああああああああああああ",
+                image: "images/S-01.png"
+            },
+        ]
+    },
+];
+
+// 体験教材
+const materials_taiken = [
+    {
+        title: "Scratchに触れる",
+        materials: [
+            {
+                id: "X1-01",
+                name: "おたからゲット！ゲーム",
+                link: "projects/x/X1-01 体験 おたからゲット！ゲーム.html",
+                image: "images/X01.png"
+            },
+        ]
+    },
+]
+
+// 初級
+const materials_syokyuu =[
+    {
+        title: "イベント",
+        materials: [
+            {
+                id: "A1-01",
+                name: "ふくわらいゲーム",
+                link: "projects/a/A1-01 ふくわらいゲーム.html",
+                image: "images/A1-01.png"
+            },
+        ]
+    },
+
+    {
+        title: "角度",
+        materials: [
+            {
+                id: "A1-02",
+                name: "チーズをうばえ！ゲーム",
+                link: "projects/a/A1-02 チーズをうばえ！ゲーム.html",
+                image: "images/A1-02.png"
+            },
+            {
+                id: "A1-03",
+                name: "パックンゲーム",
+                link: "projects/a/A1-03 パックンゲーム.html",
+                image: "images/A1-03.png"
+            },
+            {
+                id: "A1-04",
+                name: "スイカわりゲーム",
+                link: "projects/a/A1-04 スイカわりゲーム.html",
+                image: "images/A1-04.png"
+            },
+        ]
+    },
+
+    {
+        title: "座標",
+        materials: [
+            {
+                id: "A1-05",
+                name: "めいろゲーム",
+                link: "projects/a/A1-05 めいろゲーム.html",
+                image: "images/A1-05.png"
+            },
+            {
+                id: "A1-06",
+                name: "まちがいさがしゲーム",
+                link: "projects/a/A1-06 まちがいさがしゲーム.html",
+                image: "images/A1-06.png"
+            },
+            {
+                id: "A1-07",
+                name: "かせきハンターゲーム",
+                link: "projects/a/A1-07 かせきハンターゲーム.html",
+                image: "images/A1-07.png"
+            },
+        ]
+    },
+
+    {
+        title: "くりかえし",
+        materials: [
+            {
+                id: "A1-08",
+                name: "ジャンプでよけろ！ゲーム",
+                link: "projects/a/A1-08 ジャンプでよけろ！ゲーム.html",
+                image: "images/A1-08.png"
+            },
+            {
+                id: "A1-09",
+                name: "ゆきがっせんゲーム",
+                link: "projects/a/A1-09 ゆきがっせんゲーム.html",
+                image: "images/A1-09.png"
+            },
+            {
+                id: "A1-10",
+                name: "ガチャガチャゲーム",
+                link: "projects/a/A1-10 ガチャガチャゲーム.html",
+                image: "images/A1-10.png"
+            },
+            {
+                id: "A1-11",
+                name: "ぎりぎりミルクゲーム",
+                link: "projects/a/A1-11 ぎりぎりミルクゲーム.html",
+                image: "images/A1-11.png"
+            },
+        ]
+    },
+
+    {
+        title: "メッセージ",
+        materials: [
+            {
+                id: "A2-01",
+                name: "ももたろうアニメ",
+                link: "projects/a/A2-01 ももたろうアニメ.html",
+                image: "images/A2-01.png"
+            },
+            {
+                id: "A2-02",
+                name: "イライラぼうゲーム",
+                link: "projects/a/A2-02 イライラぼうゲーム.html",
+                image: "images/A2-02.png"
+            },
+            {
+                id: "A2-03",
+                name: "インベーダーゲーム",
+                link: "projects/a/A2-03 インベーダーゲーム.html",
+                image: "images/A2-03.png"
+            },
+            {
+                id: "A2-04",
+                name: "たまごをのせろ！ゲーム",
+                link: "projects/a/A2-04 たまごをのせろ！ゲーム.html",
+                image: "images/A2-04.png"
+            },
+        ]
+    },
+
+    {
+        title: "条件分岐",
+        materials: [
+            {
+                id: "A2-05",
+                name: "タイムアタックレースゲーム",
+                link: "projects/a/A2-05 タイムアタックレースゲーム.html",
+                image: "images/A2-05.png"
+            },
+            {
+                id: "A2-06",
+                name: "とりをたすけろ！ゲーム",
+                link: "projects/a/A2-06 とりをたすけろ！ゲーム.html",
+                image: "images/A2-06.png"
+            },
+            {
+                id: "A2-07",
+                name: "しわけこうじょうゲーム",
+                link: "projects/a/A2-07 しわけこうじょうゲーム.html",
+                image: "images/A2-07.png"     
+            },
+            {
+                id: "A2-08",
+                name: "おつかいゲーム",
+                link: "projects/a/A2-08 おつかいゲーム.html",
+                image: "images/A2-08.png"
+            }
+        ]
+    },
+]
+
+// 中級
+const materials_tyuukyuu=[
+    {
+        title: "変数",
+        materials: [
+            {
+                id: "B1-01",
+                name: "りんごパニック！ゲーム",
+                link: "projects/b/B1-01 りんごパニック！ゲーム.html",
+                image: "images/B1-01.png"
+            },
+            {
+                id: "B1-02",
+                name: "タイピングゲーム",
+                link: "projects/b/B1-02 タイピングゲーム.html",
+                image: "images/B1-02.png"
+            },
+            {
+                id: "B1-03",
+                name: "クリッカーゲーム",
+                link: "projects/b/B1-03 クリッカーゲーム.html",
+                image: "images/B1-03.png"
+            },
+        ]
+    },
+
+    {
+        title: "乱数",
+        materials: [
+            {
+                id: "B1-04",
+                name: "もぐらたたきゲーム",
+                link: "projects/b/B1-04 もぐらたたきゲーム.html",
+                image: "images/B1-04.png"
+            },
+            {
+                id: "B1-05",
+                name: "くるまをよけろ！ゲーム",
+                link: "projects/b/B1-05 くるまをよけろ！ゲーム.html",
+                image: "images/B1-05.png"
+            },
+            {
+                id: "B1-06",
+                name: "にんじゃジャンプゲーム",
+                link: "projects/b/B1-06 にんじゃジャンプゲーム.html",
+                image: "images/B1-06.png"
+            },
+        ]
+    },
+
+    {
+        title: "クローン",
+        materials: [
+            {
+                id: "B1-07",
+                name: "さかなつりゲーム",
+                link: "projects/b/B1-07 さかなつりゲーム.html",
+                image: "images/B1-07.png"
+            },
+            {
+                id: "B1-08",
+                name: "ユニコーンダッシュゲーム",
+                link: "projects/b/B1-08 ユニコーンダッシュゲーム.html",
+                image: "images/B1-08.png"
+            },
+            {
+                id: "B1-09",
+                name: "ブロックくずしゲーム",
+                link: "projects/b/B1-09 ブロックくずしゲーム.html",
+                image: "images/B1-09.png"
+            },
+        ]
+    },
+
+    {
+        title: "リスト",
+        materials: [
+            {
+                id: "B2-01",
+                name: "クイズゲーム",
+                link: "projects/b/B2-01 クイズゲーム.html",
+                image: "images/B2-01.png"
+            },
+            {
+                id: "B2-02",
+                name: "だっしゅつゲーム",
+                link: "projects/b/B2-02 だっしゅつゲーム.html",
+                image: "images/B2-02.png"
+            },
+            {
+                id: "B2-03",
+                name: "スロットマシンゲーム",
+                link: "projects/b/B2-03 スロットマシンゲーム.html",
+                image: "images/B2-03.png"
+            },
+        ]
+    },
+
+    {
+        title: "ブロック定義",
+        materials: [
+            {
+                id: "B2-04",
+                name: "クレーンゲーム",
+                link: "projects/b/B2-04 クレーンゲーム.html",
+                image: "images/B2-04.png"
+            },
+            {
+                id: "B2-05",
+                name: "いあいぎりゲーム",
+                link: "projects/b/B2-05 いあいぎりゲーム.html",
+                image: "images/B2-05.png"
+            },
+        ]
+    },
+
+    {
+        title: "引数",
+        materials: [
+            {
+                id: "B2-06",
+                name: "ゆうれいからにげろ！ゲーム",
+                link: "projects/b/B2-06 ゆうれいからにげろ！ゲーム.html",
+                image: "images/B2-06.png"
+            },
+            {
+                id: "B2-07",
+                name: "ボールシュートゲーム",
+                link: "projects/b/B2-07 ボールシュートゲーム.html",
+                image: "images/B2-07.png"
+            },
+            {
+                id: "B2-08",
+                name: "ふうせんわりゲーム",
+                link: "projects/b/B2-08 ふうせんわりゲーム.html",
+                image: "images/B2-08.png"
+            },
+        ]
+    },
+
+    {
+        title: "論理演算",
+        materials: [
+            {
+                id: "B3-01",
+                name: "アイスタワーゲーム",
+                link: "projects/b/B3-01 アイスタワーゲーム.html",
+                image: "images/B3-01.png"
+            },
+            {
+                id: "B3-02",
+                name: "ケーキこうじょうゲーム",
+                link: "projects/b/B3-02 ケーキこうじょうゲーム.html",
+                image: "images/B3-02.png"
+            },
+        ]
+    },
+
+    {
+        title: "真偽値",
+        materials: [
+            {
+                id: "B3-03",
+                name: "アキネーター",
+                link: "projects/b/B3-03 アキネーター.html",
+                image: "images/B3-03.png"
+            },
+            {
+                id: "B3-04",
+                name: "じゃんけんゲーム",
+                link: "projects/b/B3-04 じゃんけんゲーム.html",
+                image: "images/B3-04.png"
+            },
+        ]
+    },
+
+    {
+        title: "発展",
+        materials: [
+            {
+                id: "B3-05",
+                name: "タイミングゲーム",
+                link: "projects/b/B3-05 タイミングゲーム.html",
+                image: "images/B3-05.png"
+            },
+            {
+                id: "B3-06",
+                name: "チャリジャンプゲーム",
+                link: "projects/b/B3-06 チャリジャンプゲーム.html",
+                image: "images/B3-06.png"
+            },
+            {
+                id: "B3-07",
+                name: "パイプパズルゲーム",
+                link: "projects/b/B3-07 パイプパズルゲーム.html",
+                image: "images/B3-07.png"
+            },
+        ]
+    }
+]
+
+// 上級
+const materials_joukyuu=[
+    {
+        title: "自由にアレンジしよう！",
+        materials: [
+            {
+                id: "C1-01",
+                name: "バッティングゲーム",
+                link: "projects/c/C1-01 バッティングゲーム.html",
+                image: "images/C1-01.png"
+            },
+            {
+                id: "C1-02",
+                name: "ターンバトルゲーム",
+                link: "projects/c/C1-02 ターンバトルゲーム.html",
+                image: "images/C1-02.png"
+            },
+            {
+                id: "C1-03",
+                name: "じんとりバトルゲーム",
+                link: "projects/c/C1-03 じんとりバトルゲーム.html",
+                image: "images/C1-03.png"
+            },
+            {
+                id: "C2-01",
+                name: "タワーディフェンスゲーム",
+                link: "projects/c/C2-01 タワーディフェンスゲーム.html",
+                image: "images/C2-01.png"
+            },
+            {
+                id: "C2-02",
+                name: "2Dアクションゲーム",
+                link: "projects/c/C2-02 2Dアクションゲーム.html",
+                image: "images/C2-02.png"
+            },
+            {
+                id: "C2-03",
+                name: "わざアクションゲーム",
+                link: "projects/c/C2-03 わざアクションゲーム.html",
+                image: "images/C2-03.png"
+            }
+        ]
+    }
+]
+
+//ページ読み込み時の処理
+document.addEventListener("DOMContentLoaded", ()=>{
+  // 教材用テーブル描画
+  renderSections(materials_taiken, "table-taiken", false);
+  renderSections(materials_syokyuu, "table-syokyuu", false);
+  renderSections(materials_tyuukyuu, "table-tyuukyuu", false);
+  renderSections(materials_joukyuu, "table-joukyuu", false);
+
+  //生徒作品用テーブル描画
+  renderSections(materials_students, "table-students", true);
+
+  switchTab('text');
+});
+
+//タブ切り替え関数
+function switchTab(tabName){
+  // コンテンツエリアを取得
+  const textContent=document.getElementById('content-text');
+  const studentsContent=document.getElementById('content-students');
+  //ボタンを取得
+  const textBtn=document.getElementById('tab-text');
+  const studentsBtn=document.getElementById('tab-students');
+
+  const jumpToContainer=document.getElementById('jump-to-container');
+
+  if(tabName==='text'){
+    //教材を表示、生徒作品を非表示
+    textContent.classList.remove('hidden');
+    studentsContent.classList.add('hidden');
+    
+    //教材タブ中はJump to...を表示
+    if(jumpToContainer) jumpToContainer.classList.remove('hidden');
+
+    //ボタンのスタイルを変更
+    setActiveTabButtonStyle(textBtn);
+    setInactiveTabButtonStyle(studentsBtn);
+  }else if(tabName==='students'){
+    //生徒作品を表示、教材を非表示
+    studentsContent.classList.remove('hidden');
+    textContent.classList.add('hidden');
+
+    //生徒作品タブ中はJump to...非表示
+    if(jumpToContainer) jumpToContainer.classList.add('hidden');
+
+    //ボタンのスタイルを変更
+    setActiveTabButtonStyle(studentsBtn);
+    setInactiveTabButtonStyle(textBtn);
+  }
+}
+
+//選択中タブ
+function setActiveTabButtonStyle(btn){
+  btn.className="tab-btn px-5 py-2.5 rounded-t-lg font-bold border border-b-0 border-[#ddd] bg-white text-[#333] relative z-20 transition-all";
+}
+
+//未選択タブ
+function setInactiveTabButtonStyle(btn){
+  btn.className="tab-btn px-5 py-2.5 rounded-t-lg font-bold border border-b-0 border-[#ddd] bg-[#e6f0fa] text-[#007bff] hover:bg-[#d8e8f8] relative z-0 transition-all";
+}
+
+function renderSections(sections, containerId, isStudent=false){
+  const container=document.getElementById(containerId);
+  if(!container) return;
+
+  container.replaceChildren();
+  container.classList.add('space-y-8');
+
+  const sectionColors={
+    'table-syokyuu': 'border-green-200 bg-green-100',
+    'table-tyuukyuu': 'border-yellow-200 bg-yellow-100',
+    'table-joukyuu': 'border-red-200 bg-red-100'
+  };
+  const sectionColor=sectionColors[containerId] || 'border-slate-200 bg-slate-100';
+
+  sections.forEach((section, index)=>{
+    const sectionElement=document.createElement('section');
+    sectionElement.className=`border ${sectionColor} rounded-lg p-4`;
+    const heading=document.createElement('h3');
+    heading.className='text-xl font-bold mb-3';
+    heading.textContent=section.title;
+
+    const cardsContainer=document.createElement('div');
+    cardsContainer.id=`${containerId}-section-${index}`;
+
+    sectionElement.append(heading, cardsContainer);
+    container.append(sectionElement);
+    renderCards(section.materials, cardsContainer.id, isStudent);
+  });
+}
+
+function renderCards(dataList, containerId, isStudent=false){
+  const container=document.getElementById(containerId);
+  if(!container) return;
+
+  const cardHtml=dataList.map(item=>{
+    if(isStudent){
+      return `
+        <div class="flex items-start w-full min-w-0 h-full bg-white border-2 border-[#bbb] rounded-lg overflow-hidden shadow-sm">
+            <div class="w-1/3 max-w-[160px] shrink-0 border-r border-[#ddd]">
+                <a href="${encodeURI(item.link)}" target="_blank" rel="noopener noreferrer"
+                   class="block w-full aspect-[4/3] bg-slate-100 border-b border-[#ddd] overflow-hidden hover:opacity-80 transition-opacity">
+                    <img src="${item.image || 'images/placeholder.png'}" alt="${item.name}" class="w-full h-full object-cover">
+                </a>
+                <div class="p-2 text-xs [overflow-wrap:anywhere]">
+                    <p class="mb-1 text-[11px] text-gray-600">作品名</p>
+                    <p>${item.name}</p>
+                </div>
+            </div>
+            <dl class="flex-1 min-w-0 p-3 space-y-3 text-xs leading-relaxed [overflow-wrap:anywhere]">
+                <div>
+                    <dt class="mb-1 font-bold text-gray-600">校舎・教室</dt>
+                    <dd>${item.school || ''}</dd>
+                </div>
+                <div>
+                    <dt class="mb-1 font-bold text-gray-600">生徒名</dt>
+                    <dd>${item.student_name || ''}</dd>
+                </div>
+                <div>
+                    <dt class="mb-1 font-bold text-gray-600">作品メモ</dt>
+                    <dd class="whitespace-pre-wrap">${item.memo || ''}</dd>
+                </div>
+            </dl>
+        </div>
+      `;
+    }
+
+    return `
+        <div class="flex flex-col w-full max-w-[160px] h-full bg-white border-2 border-[#bbb] rounded-lg overflow-hidden shadow-sm">
+            <a href="${encodeURI(item.link)}" target="_blank" rel="noopener noreferrer"
+            class="block w-full aspect-[4/3] bg-slate-100 border-b border-[#ddd] overflow-hidden hover:opacity-80 transition-opacity">
+                <img src="${item.image || 'images/placeholder.png'}" alt="${item.name}" class="w-full h-full object-cover">
+            </a>
+
+            <table class="w-full flex-1 h-full border-collapse text-left">
+                <thead>
+                    <tr class="bg-[#f8f8f8] text-[11px] font-bold">
+                        <th class="border border-[#ddd] px-2 py-0.5 text-left">${isStudent?'作品名':'教材名'}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="border border-[#ddd] px-2 py-1 text-left text-xs leading-snug">${item.name}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    `;
+  }).join("");
+
+  const gridClass=isStudent
+    ? 'grid grid-cols-1 lg:grid-cols-2 gap-4'
+    : 'grid max-w-[1020px] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3';
+
+  container.innerHTML=`
+    <div class="${gridClass}">
+        ${cardHtml}
+    </div>
+  `;
+}
